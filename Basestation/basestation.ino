70449dcc2a9a6238cddc671915526bb2eeba849e
Tijam Moradi <tijam.moradi@cgi.com>
2019-10-14 18:37:26 +0300

    Removed test code

--- Basestation/basestation.ino
+++ Basestation/basestation.ino
@@ -1,23 +1,19 @@
 #include <stdio.h>
 
 void setup() {
-  // initialize digital pin LED_BUILTIN as an output.
-  pinMode(LED_BUILTIN, OUTPUT);
   Serial.begin(9600);
+
+  // Setup inputs
+  
 }
 
 // the loop function runs over and over again forever
 void loop() {
-  digitalWrite(LED_BUILTIN, HIGH);   // turn the LED on 
-  sendTelemetry("Led", "ON");
-  Serial.println("asdad");
-  delay(100);                       // wait for half a second
-  digitalWrite(LED_BUILTIN, LOW);    // turn the LED off 
-  delay(100);                       // wait for half a second
+
 }
 
 // Sends the telemetry to Serial with nice logging
 void sendTelemetry(String key, String value) {
-  String messageLog = (String) "[" + millis() + "] " + key + ": " + value;
+  String messageLog = (String) "[ INFO ] " + key + ": " + value;
   Serial.println(messageLog);
 }
