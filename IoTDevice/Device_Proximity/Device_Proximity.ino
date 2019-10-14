756cfc44e783fb4c241625550cb7a7aa36fa396f
Tijam Moradi <tijam.moradi@cgi.com>
2019-10-14 20:54:30 +0300

    Proximity sensor is done

--- IoTDevice/Device_Proximity/Device_Proximity.ino
+++ IoTDevice/Device_Proximity/Device_Proximity.ino
@@ -1,9 +1,56 @@
-void setup() {
-  // put your setup code here, to run once:
+// defines pins numbers
+const int trigPin = 6;
+const int echoPin = 5;
 
+// defines variables
+long duration;
+int distance;
+
+class MessageFormat {
+  public:
+  struct Telemetry {
+    String key;
+    float value;
+  };
+};
+
+void sendTelemetry(MessageFormat::Telemetry t) {
+  Serial.println((String) t.key + ": " + t.value);
+
+  // TODO implement RF logic
+}
+
+void setup() {
+  pinMode(trigPin, OUTPUT); // Sets the trigPin as an Output
+  pinMode(echoPin, INPUT); // Sets the echoPin as an Input
+  Serial.begin(9600); // Starts the serial communication
 }
 
 void loop() {
-  // put your main code here, to run repeatedly:
+  delay(2000);
+  int distance = getDistanceInCm();
 
+  MessageFormat::Telemetry telemetry = {
+    "Distance",
+    (float) distance
+  };
+  
+  sendTelemetry(telemetry);
+}
+
+// Reads the proximity sensor 
+int getDistanceInCm() {
+  // Clears the trigPin
+  digitalWrite(trigPin, LOW);
+  delayMicroseconds(2);
+  // Sets the trigPin on HIGH state for 10 micro seconds
+  digitalWrite(trigPin, HIGH);
+  delayMicroseconds(10);
+  digitalWrite(trigPin, LOW);
+  // Reads the echoPin, returns the sound wave travel time in microseconds
+  duration = pulseIn(echoPin, HIGH);
+  // Calculating the distance
+  distance = duration*0.034/2;
+  
+  return distance;
 }
